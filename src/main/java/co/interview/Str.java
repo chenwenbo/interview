2c43ae33087847299e17d1296bc0a0d56245515f
chenwenbo <594017622@qq.com>
2016-09-12 14:18:48 +0800

    add string convert num case

---
+++ src/main/java/co/interview/Str.java
@@ -0,0 +1,19 @@
+package co.interview;
+
+/**
+ * convert String to number
+ */
+public class Str {
+
+    public static final char ZERO_CHAR = '0';
+
+    public int convert(String str) {
+
+        int result = 0;
+        for (int i = 0; i < str.length(); i++) {
+           result = result * 10 + str.charAt(i)-ZERO_CHAR;
+        }
+        return result;
+    }
+
+}
