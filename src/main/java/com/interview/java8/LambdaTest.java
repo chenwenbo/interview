04830151bd4f567b9cdd821111f7775d2bc7d415
chenwenbo <594017622@qq.com>
2016-10-28 13:46:53 +0800

    add lambda expression case

---
+++ src/main/java/com/interview/java8/LambdaTest.java
@@ -0,0 +1,38 @@
+package com.interview.java8;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * chen wenbo
+ * Created by apple on 2016/10/28 12:58.
+ */
+public class LambdaTest {
+    public static void main(String[] args) {
+        List<String> strs = Arrays.asList("aaa", "bbb");
+        strs.forEach(c -> System.out.println(c));
+        strs.forEach(c -> {
+            if (c.equals("aaa")) {
+                System.out.println("bingo");
+            }
+        });
+
+        Map<String, String> map = new HashMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key3", "value3");
+        map.forEach((k, v) -> System.out.println(k + v));
+
+        strs.stream().filter(c -> c.equals("aaa")).forEach(System.out::println);
+
+        String result = map.entrySet().stream()
+                .filter(c -> !c.getKey().equals("key1"))
+                .map(c -> c.getKey())
+                .collect(Collectors.joining(","));
+        System.out.println(result);
+
+        List<Integer> numbers = Arrays.asList(1, 1, 3, 4);
+        numbers.stream().distinct().forEach(System.out::print);
+
+    }
+}
